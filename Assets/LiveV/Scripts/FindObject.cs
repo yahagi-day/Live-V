b295068cb22d28982eb02b7d97763e7380550d81
yahagi_day <yahagi.day@tensi.org>
2019-06-10 19:35:14 +0900

    身長差補正を実装

---
+++ Assets/LiveV/Scripts/FindObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Live_V
+{
+    public class FindObject : MonoBehaviour
+    {
+        public GameObject FindGameObject()
+        {
+            return transform.gameObject;
+        }
+
+    }
+}
