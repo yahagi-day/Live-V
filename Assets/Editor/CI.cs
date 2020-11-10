8a02b6642fff3ba996f7c6e60d4da0327320e1ec
yahagi_day <yahagi.day@tensi.org>
2020-11-10 16:23:51 +0900

    Add Build Scripts

---
+++ Assets/Editor/CI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using System.Linq;
+
+public class CI
+{
+    [MenuItem("CI/Build")]
+    public static void Build()
+    {
+        BuildPipeline.BuildPlayer(ScenePaths, "Build/Live-V.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    private static string[] ScenePaths => EditorBuildSettings.scenes.Select(scene => scene.path)
+        .ToArray();
+}
