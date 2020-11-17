5bc6e4c2c7ddff0a5df76c7d63b8662b160ab3ec
yahagi_day <yahagi.day@tensi.org>
2020-11-17 16:47:54 +0900

    Add Disable gameobject when WebGL Build

---
+++ Assets/LiveV/Scripts/DisableAudio.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DisableAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+#if UNITY_WEBGL
+        gameObject.SetActive(false);
+#endif
+    }
+}
