8459d87e60114a84767cded9c987046065a69af5
yahagi_day <yahagi.day@tensi.org>
2020-11-17 16:48:15 +0900

    Update workflow

--- Assets/Editor/CI.cs
+++ Assets/Editor/CI.cs
@@ -3,10 +3,16 @@ using System.Linq;
 
 public class CI
 {
-    [MenuItem("CI/Build")]
-    public static void Build()
+    [MenuItem("CI/WindowsBuild")]
+    public static void WindowsBuild()
     {
         BuildPipeline.BuildPlayer(ScenePaths, "Build/Live-V.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    [MenuItem("CI/WebGLBuild")]
+    public static void WebGLBuild()
+    {
+        BuildPipeline.BuildPlayer(ScenePaths, "docs", BuildTarget.WebGL, BuildOptions.None);
     }
 
     private static string[] ScenePaths => EditorBuildSettings.scenes.Select(scene => scene.path)
